4a102647dc0fdb45f3a1e321571aa9c9d2f4ad6f
Tormented <69302186+TormentedProgram@users.noreply.github.com>
2025-06-20 17:54:14 +1000

    oh so you have to embed the wav into the exe its not automatic I see

--- src/utils.rs
+++ src/utils.rs
@@ -1,27 +1,41 @@
-use std::io::BufReader;
+use std::fs;
 use std::path::PathBuf;
-use std::process::{Command, Stdio};
+use std::process::{Command};
+use rust_embed::Embed;
+
+#[derive(Embed)]
+#[folder = "assets/"]
+#[prefix = "assets/"]
+struct Asset;
 
 pub fn copy_video_file(file_path:&PathBuf) {
-    let video_file_path = file_path.to_str().unwrap().to_string();
+    match fs::canonicalize(&file_path) {
+        Ok(absolute_path) => {
+            let video_file_path = absolute_path.to_str().unwrap().to_string();
 
-    let command = format!("echo -n 'file://{}' | wl-copy -t text/uri-list", video_file_path);
+            let command = format!("echo -n 'file://{}' | wl-copy -t text/uri-list", video_file_path);
 
-    let status = Command::new("sh")
-        .arg("-c")
-        .arg(command)
-        .status()
-        .expect("Failed to execute command");
+            let status = Command::new("sh")
+                .arg("-c")
+                .arg(command)
+                .status()
+                .expect("Failed to execute command");
 
-    if !status.success() {
-        eprintln!("Command failed with status: {}", status);
-    }else{
-        println!("Video successfully copied to clipboard..");
+            if !status.success() {
+                eprintln!("Command failed with status: {}", status);
+            }else{
+                println!("Video successfully copied to clipboard..");
+            }
+        }
+        Err(e) => {
+            eprintln!("Error converting to absolute path: {}", e);
+        }
     }
 
     let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-    let file = std::fs::File::open("assets/done.wav").unwrap();
-    let beep1 = stream_handle.play_once(BufReader::new(file)).unwrap();
+    let embedded_file = Asset::get("assets/done.wav").unwrap();
+    let cursor = std::io::Cursor::new(embedded_file.data); // Use the correct field to access the data
+    let beep1 = stream_handle.play_once(cursor).unwrap();
     beep1.set_volume(0.2);
     beep1.sleep_until_end();
     drop(beep1);
